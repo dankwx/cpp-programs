3d6e24b349071e18bbd5864d3189e8c1e42a42f1
Daniel Kondlatsch <daniel_pettres@sicredi.com.br>
2023-05-10 06:37:24 -0300

    Conversor de moeda Dolar para Real finalizado

--- ConversorMoeda/main.cpp
+++ ConversorMoeda/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
+    float valorReal;
+
+    std::cout << "Insira o valor em reais que deseja converter para Dólar." << std::endl;
+    std::cin >> valorReal;
+
+    std::cout << "O valor inserido convertido em Dólar é de: R$" << std::fixed << std::setprecision(2) << valorReal / 5 << " Reais."
+              << std::endl;
 
     return 0;
 }
